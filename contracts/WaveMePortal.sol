6f261a1046dd803b5051383961f0aaa3f280ef1a
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-30 17:04:59 +0530

    Cleaning up the folders, and Smart Contract File created

---
+++ contracts/WaveMePortal.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract WaveMePortal {
+    constructor() {
+     //We can use console.log inside smart contract, because of hardhat
+        console.log("YoYo! I am contract and I am smart");
+    }
+}
