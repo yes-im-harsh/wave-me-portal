b32ac922b4c36795429fe50f7cb3c6056172ba64
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-10-01 21:33:26 +0530

    Store "👋" data on our smart contract

--- contracts/WaveMePortal.sol
+++ contracts/WaveMePortal.sol
@@ -5,8 +5,20 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WaveMePortal {
+    uint256 totalWaves;
+
     constructor() {
-     //We can use console.log inside smart contract, because of hardhat
+        //We can use console.log inside smart contract, because of hardhat
         console.log("YoYo! I am contract and I am smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,11 +1,26 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory(
     "WaveMePortal"
   );
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
 
-  console.log("Wave Deployed to:", waveContract.address);
+  console.log("Contract Deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  await waveContract.getTotalWaves();
+
+  const firstWaveTxn = await waveContract.wave();
+  await firstWaveTxn.wait();
+
+  await waveContract.getTotalWaves();
+
+  // For getting a wave from anyone other than you
+  const secondWaveTxn = await waveContract.connect(randomPerson).wave();
+  await secondWaveTxn.wait();
+
+  await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
