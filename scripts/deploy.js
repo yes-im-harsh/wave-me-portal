e7835fc30bcaf7034d3d1a33633d0e87ddca1ebc
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-10-01 22:18:11 +0530

    Deployed locally so we can start building the website

---
+++ scripts/deploy.js
@@ -0,0 +1,29 @@
+const main = async () => {
+  const [deployer] = await hre.ethers.getSigners();
+  const accountBalance = await deployer.getBalance();
+
+  console.log("Deploying contract with account:", deployer.address);
+  console.log("Account balance:", accountBalance.toString());
+
+  const waveContractFactory = await hre.ethers.getContractFactory(
+    "WaveMePortal"
+  );
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+
+  console.log("WaveMePortal address: ", waveContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
+
+//command to deploy locally is: npx hardhat run scripts/deploy.js --network localhost
